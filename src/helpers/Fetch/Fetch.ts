83544d96cfc21b94c6ad3cdd363407abde59dbb7
Zetharion <zetharionshim@gmail.com>
2024-08-19 01:19:36 +0300

    refactor: removed catch block and casting

--- src/helpers/Fetch/Fetch.ts
+++ src/helpers/Fetch/Fetch.ts
@@ -4,14 +4,8 @@ export const Fetch = async <T>(
 	input: string,
 	fetchMode: FetchModes,
 	init?: RequestInit
-): Promise<T> => {
-	return await fetch(input, init && init)
-		.then(response => {
-			return fetchMode === FetchModes.JSON
-				? (response.json() as Promise<T>)
-				: (response.text() as Promise<T>)
-		})
-		.catch(err => {
-			throw new Error(err)
-		})
+): Promise<T | unknown> => {
+	return await fetch(input, init).then(response => {
+		return fetchMode === FetchModes.JSON ? response.json() : response.text()
+	})
 }
