8a228d2cd7cb206068d7163fb199341a5af72682
Zetharion <zetharionshim@gmail.com>
2024-08-18 23:39:38 +0300

    refactor: fetch implementation

--- src/helpers/Fetch/Fetch.ts
+++ src/helpers/Fetch/Fetch.ts
@@ -1,19 +1,17 @@
 import { FetchModes } from './Fetch.types'
 
-export const Fetch = async (
+export const Fetch = async <T>(
 	input: string,
 	fetchMode: FetchModes,
 	init?: RequestInit
-) => {
-	const response = await fetch(input, init && init)
-		.then(res => {
-			if (fetchMode === FetchModes.TEXT) {
-				return res.text()
-			}
-			return res.json()
+): Promise<T> => {
+	return await fetch(input, init && init)
+		.then(response => {
+			return fetchMode === FetchModes.JSON
+				? (response.json() as Promise<T>)
+				: (response.text() as Promise<T>)
 		})
 		.catch(err => {
 			throw new Error(err)
 		})
-	return response
 }
