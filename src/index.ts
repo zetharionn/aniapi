8a8ac62589e6e4b52a4cfea9b121c9b642d43423
Zetharion <zetharionshim@gmail.com>
2024-08-19 02:50:40 +0300

    refactor: exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export * from './api/Anilibria'
+export { Anilibria, Endpoints, URL } from './api/Anilibria'
+export type {
+	IAnilibria,
+	Title,
+	Names,
+	Franchises,
+	Franchise,
+	Posters,
+	Poster,
+	Release,
+	Season,
+	SeasonCode,
+	Status,
+	StatusCodes,
+	Team,
+	Type,
+	TypeCodes
+} from './api/Anilibria'
