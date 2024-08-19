0edf3ab086dc36d1eb3ecc5fe0ecb029e1ea25a1
Zetharion <zetharionshim@gmail.com>
2024-08-19 15:37:35 +0300

    refactor: casting

--- src/helpers/Fetch/Fetch.ts
+++ src/helpers/Fetch/Fetch.ts
@@ -4,8 +4,10 @@ export const Fetch = async <T>(
 	input: string,
 	fetchMode: FetchModes = FetchModes.JSON,
 	init?: RequestInit
-): Promise<T | unknown> => {
+): Promise<T> => {
 	return await fetch(input, init).then(response => {
-		return fetchMode === FetchModes.JSON ? response.json() : response.text()
+		return fetchMode === FetchModes.JSON
+			? (response.json() as Promise<T>)
+			: (response.text() as Promise<T>)
 	})
 }
