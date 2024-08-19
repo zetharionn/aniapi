816b5983c5b19ce6a169b1b4616da6aaaf65aa74
Zetharion <zetharionshim@gmail.com>
2024-08-19 17:20:51 +0300

    refactor: instance types

--- src/api/Anilibria/anilibria.types.ts
+++ src/api/Anilibria/anilibria.types.ts
@@ -1,6 +1,6 @@
 import { Title } from './types'
 
 export interface IAnilibria {
-	getTitle: (id: number) => Promise<Title | unknown>
-	getRandomTitle: () => Promise<Title | unknown>
+	getTitle: (id: number) => Promise<Title>
+	getRandomTitle: () => Promise<Title>
 }
