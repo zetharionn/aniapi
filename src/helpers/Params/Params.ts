939dbf80fd5414d508813bc527435d8b449e4d4e
Zetharion <zetharionshim@gmail.com>
2024-08-21 20:46:12 +0300

    refactor: params helper

--- src/helpers/Params/Params.ts
+++ src/helpers/Params/Params.ts
@@ -1,7 +1,6 @@
-export const Params = <T extends object>(params: T): string => {
+export const Params = <T extends object>(paramsObject: T): string => {
 	const paramsString: string[] = []
-
-	for (const [key, value] of Object.entries(params)) {
+	for (const [key, value] of Object.entries(paramsObject)) {
 		paramsString.push(
 			`${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`
 		)
