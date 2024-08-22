899a3af6202b8554a7d0f27253e383a9a2a1c28c
Zetharion <zetharionshim@gmail.com>
2024-08-22 23:18:34 +0300

    refactor: class types

--- src/api/Anilibria/anilibria.types.ts
+++ src/api/Anilibria/anilibria.types.ts
@@ -1,12 +1,12 @@
 import { GetAnilibriaRandomTitleParams } from './methods/getAnilibriaRandomTitle'
 import { GetAnilibriaTitleParams } from './methods/getAnilibriaTitle'
 import { GetAnilibriaTitleListParams } from './methods/getAnilibriaTitleList'
-import { Title } from './types'
+import { Title, Titles } from './types'
 import { GetAnilibriaSearchTitleParams } from './methods/getAnilibriaSearchTitle/getAnilibriaSearchTitle.types'
 
 export interface IAnilibria {
-	getTitle: (params: GetAnilibriaTitleParams) => Promise<Title>
-	getRandomTitle: (params: GetAnilibriaRandomTitleParams) => Promise<Title>
-	getTitleList: (params: GetAnilibriaTitleListParams) => Promise<Title[]>
-	getSearchTitle: (params: GetAnilibriaSearchTitleParams) => Promise<Title[]>
+	getTitle: (params?: GetAnilibriaTitleParams) => Promise<Title>
+	getRandomTitle: (params?: GetAnilibriaRandomTitleParams) => Promise<Title>
+	getTitleList: (params?: GetAnilibriaTitleListParams) => Promise<Titles>
+	getSearchTitle: (params?: GetAnilibriaSearchTitleParams) => Promise<Titles>
 }
