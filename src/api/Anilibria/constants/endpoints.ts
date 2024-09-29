6819544edcf45dc9011e7065596924bc4e0def8b
zetharionn <zetharionshim@gmail.com>
2024-09-29 13:50:09 +0300

    refactor: endpoints

--- src/api/Anilibria/constants/endpoints.ts
+++ src/api/Anilibria/constants/endpoints.ts
@@ -1,16 +1,24 @@
 import { URI } from './uri'
 
-export const enum Endpoints {
-	APP = URI.ANILIBRIA_API_URI + '/app',
-	APP_SEARCH = APP + '/search/release',
-	ANIME = URI.ANILIBRIA_API_URI + '/anime',
-	GENRES = ANIME + '/genres',
-	GENRES_RANDOM = GENRES + '/random',
-	RELEASE = ANIME + '/releases',
-	RELEASE_LATEST = RELEASE + '/latest',
-	RELEASE_RANDOM = RELEASE + '/random',
-	RELEASE_EPISODES = RELEASE + '/episodes',
-	SCHEDULE = ANIME + '/schedule',
-	SCHEDULE_NOW = SCHEDULE + '/now',
-	SCHEDULE_WEEK = SCHEDULE + '/week'
+const APP = `${URI.ANILIBRIA_API_URI}/app`
+const ANIME = `${URI.ANILIBRIA_API_URI}/anime`
+const CATALOG = `${ANIME}/catalog`
+const GENRES = `${ANIME}/genres`
+const RELEASE = `${ANIME}/releases`
+const SCHEDULE = `${ANIME}/schedule`
+
+export const Endpoints = {
+	APP,
+	ANIME,
+	CATALOG,
+	GENRES,
+	RELEASE,
+	SCHEDULE,
+	APP_SEARCH: `${APP}/search/release`,
+	GENRES_RANDOM: `${GENRES}/random`,
+	RELEASE_LATEST: `${RELEASE}/latest`,
+	RELEASE_RANDOM: `${RELEASE}/random`,
+	RELEASE_EPISODES: `${RELEASE}/episodes`,
+	SCHEDULE_NOW: `${SCHEDULE}/now`,
+	SCHEDULE_WEEK: `${SCHEDULE}/week`
 }
